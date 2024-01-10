76420874e50f83b8695d60b45aef07101118402a
NeilRanawake <neilranawake@yahoo.com>
2024-01-10 19:04:19 +1000

    Image added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
 'use client'
-import { TextInput, Box, Text, Button, Group, Combobox, useCombobox, CloseButton, Drawer  } from "@mantine/core";
 import { useDisclosure } from '@mantine/hooks';
 import { useState } from 'react';
 import React from 'react';
 import Header from '../public/components/Header'
-
+import { BackgroundImage, Center, Text, Box, Image  } from '@mantine/core';
 
 
 
@@ -16,11 +15,25 @@ export default function IndexPage() {
     <div>
     <Header />
     </div>
-      
-   
       <h1>Home Page</h1>
+      <Box maw='auto' mx="auto">
+      <BackgroundImage
+        src="/stairs.jpg"
+        radius="sm"
+        h='50vh'
+        
+      >
+        <Center p="md" style={{ textAlign: 'center'}}>
+          <Text c="red">
+          Buddha Dhamma School International is not just another Dhamma School. It aims at a specific purpose. Spiritual progress of every individual right up to enlightenment is our aim
+          </Text>
+        </Center>
+      </BackgroundImage>
+      
+    </Box>
+
     
-      </>   
+  </>   
     );
   }
 
